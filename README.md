21be0fb7840b86a530eb2187b15277b5e4fadf93
Karol Polakowski <karpol1245@gmail.com>
2019-08-14 13:14:02 +0200

    Added more examples

--- README.md
+++ README.md
@@ -15,5 +15,5 @@ Tasks done during tutoring will be stored in folders **JS-XX** or **HTML-CSS-XX*
 ## Solutions
 Almost always I will attach solutions to the tasks. If you have some problems than have a look how I did it, maybe it will help. Just don't copy the whole solution...
 
-**In case of questions write me on slack**
+*In case of questions or task requests write me on slack or drop an email karol.polakowski@digitalcareerinstitute.org*
 
--- examples-JS/logical-operators.js
+++ examples-JS/logical-operators.js
@@ -17,10 +17,18 @@ console.log(!myvalue);
 // IT CONVERTS ANY VALUE TO BOOLEAN
 console.log(!!myvalue);
 
-// COMPARE JUST VALUE
+// COMPARING JUST VALUE
 console.log(myvalue == true);
 console.log(mynumber == "1");
 
 // COMPARING VALUE AND TYPE
 console.log(myvalue === false);
 console.log(mynumber === "1");
+
+// COMPARING STRINGS
+console.log("a" > "b");
+console.log("a" > "ab");
+console.log("aa" < "ab");
+console.log("b" > "a");
+console.log("a" > "a");
+console.log("a" === "a");
