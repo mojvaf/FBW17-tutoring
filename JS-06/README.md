e6e15d42c952529ebc558399c9e913d2ba4a037d
pulamc <karpol1245@gmail.com>
2019-08-26 15:57:11 +0200

    26/08 JS Exercises

--- JS-06/README.md
+++ JS-06/README.md
@@ -1,10 +1,3 @@
 # 26/08 Javascript exercises
-1. Theory (array methods)
-    - slice()
-    - splice()
-    - split()
-    - join()
-    - shift()
-    - unshift()
-2. Find the bugs
-3. Code exercises
+1. Find the bugs in *findBugs.js* file
+2. Code exercises
--- JS-06/main.js
+++ JS-06/main.js
@@ -1,8 +1,9 @@
 let numbers = [ 2, 4, 6, 8, 10 ]
+let words = "two four six eight ten";
 
-//slice
-//splice
-//split
-//join
-//shift
-//unshift
+
+// create a function that takes a number as an argument and it print the spelled version of word (2 should print "two")
+
+
+// create a function that return array of objects, each object should have two properties, value and description, filled with proper corresponding values 
+// [ { value: 2, description: "two" }, ... , { value: 10, description: "ten" } ]
