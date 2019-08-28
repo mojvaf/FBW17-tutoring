becf610502fd3be50f216d9266b3883ca9c735f1
pulamc <karpol1245@gmail.com>
2019-08-28 23:44:42 +0200

    28/08 JS Exercisises and some fixed bugs

--- test-basics.js
+++ test-basics.js
@@ -1,18 +1,27 @@
+let numbers = [1, 2, 3, 4, 5];
+// NUMBERS 
 // #1 Write a function that print to the console each element of an array
-// #2 Write a function that return sum of two numbers
-// #3 Write a function that return sum of all numbers in array
-// #4 Write a function that return average of all numbers in array
-// #5 Write a function that return the biggest number in array
-// #6 
-// #7
-// #8
-// #9
-// #10
+// #2 Write a function that returns sum of two numbers
+// #3 Write a function that returns sum of all numbers in array
+// #4 Write a function that returns average of all numbers in array
+// #5 Write a function that returns the biggest number in array
+
+let words = ["this","is","my","array","of","strings"];
+// STRINGS 
+// #6 Write a function that display to the console "Hello _______", where blank space should be filled with name provided as argument 
+// #7 Write a function that sum lengths of each word in array and returns it
+// #8 Write a function that take WORDS array as argument and returns it as a sentence, with first letter uppercase and dot(.) at the end
+// #9 Add word 'amazing' right after third element.
+// #10 Write a function that returns the longest string in array WORDS
+
+//BOOLEANS
 // #11
 // #12
 // #13
 // #14
 // #15
+
+//OBJECTS
 // #16
 // #17
 // #18
