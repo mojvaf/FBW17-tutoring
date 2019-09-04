fbcb695b76236a055809ca27bb08372e98fffe8a
Karol Polakowski <karpol1245@gmail.com>
2019-09-04 14:59:52 +0200

    04/09 JS exercises

---
+++ JS-08/main.js
@@ -0,0 +1,32 @@
+let numbers = [ 2, 19, 18, 5, 9, 1, 10, 3 ];
+
+/* #1 write a function that takes array of numbers as argument, and returns object that will tell me how many odds and even numbers there are inside
+This is how result should look:
+{
+    odd:5,
+    even:3
+} */
+
+let mixedArray = [2, 4, "name", true, [], "tutor", 5, {name:"Karol"}];
+/* #2 Write a function takes array as argument and returns object that will tell me how many elements of each type this array contains
+{
+    number: 3,
+    string: 2,
+    boolean: 1,
+    object: 2
+} */
+
+
+let people = [
+    { name:"Karol", age:20, country: "Poland" },
+    { name:"Robert", age:25, country: "Germany" },
+    { name:"Martina", age:22, country: "Germany" },
+    { name:"Ajil", age:30, country: "Syria" },
+    { name:"Mor", age:23, country: "Israel" }
+]
+
+/* 
+#3 Write a function that takes three arguments - name of the person, property name and value that should be updated for this property, for example updateProperty("Karol","country","Germany");
+ ...
+{ name:"Karol", age:20, country: "Germany" },
+...*/
